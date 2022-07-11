c92128adfa25ad93eb51c49d709846056dccb715
palmersola <palmersola@yahoo.com>
2022-07-11 16:38:46 -0500

    added uv color and icons to each card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const APIKey = "1d19cda4c60cdb1c9b264ede49ac9a5e";
 let ls = JSON.parse(localStorage.getItem("Recent Searches"));
 let recentSearches = [];
-console.log(ls);
 if (ls === null) {
   recentSearches = [
     "Previous City",
@@ -35,6 +34,7 @@ $(".btn-secondary").click(function(event) {
   event.preventDefault();
   setWeather($("#" + event.target.id).text());
 });
+
 function setWeather(city) {
   let queryURL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
@@ -50,12 +50,18 @@ function setWeather(city) {
       lat +
       "&lon=" +
       lon +
-      "&appid=1d19cda4c60cdb1c9b264ede49ac9a5e&units=imperial&exclude=hourly,minutely";
+      "&appid=" +
+      APIKey +
+      "&units=imperial&exclude=hourly,minutely";
     fetch(queryURL2).then(response => response.json()).then(data => {
       for (i = 0; i <= 5; i++) {
         let date = new Date(data.daily[i].dt * 1000);
         let dateFormat = date.toLocaleDateString();
+        let img = data.daily[i].weather[0].icon;
         $("#" + i).children("h3").text(dateFormat);
+        $("#" + i)
+          .children("img")
+          .attr("src", "http://openweathermap.org/img/wn/" + img + "@2x.png");
         $("#" + i).children(".temp").text("Temp: " + data.daily[i].temp.day);
         $("#" + i).children(".wind").text("Wind: " + data.daily[i].wind_speed);
         $("#" + i)
@@ -63,7 +69,14 @@ function setWeather(city) {
           .text("Humidity: " + data.daily[i].humidity + "%");
         if (i === 0) {
           $("#0").children("h2").text(city + " " + dateFormat);
-          $("#0").children(".UV").text("UV Index: " + data.daily[i].uvi);
+          $("#0").children(".UV").text("UV Index: " + data.daily[0].uvi);
+        }
+        if (data.daily[0].uvi <= 4) {
+          $("#0").children(".UV").css("background-color", "green");
+        } else if (data.daily[0].uvi <= 8) {
+          $("#0").children(".UV").css("background-color", "yellow");
+        } else if (data.daily[0].uvi > 8) {
+          $("#0").children(".UV").css("background-color", "red");
         }
       }
     });
