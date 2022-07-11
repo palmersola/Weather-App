f4bd5b3222a75781a07efc333d686cab55115240
palmersola <palmersola@yahoo.com>
2022-07-11 15:06:03 -0500

    worked out api issues and added css styling

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+const APIKey = "1d19cda4c60cdb1c9b264ede49ac9a5e";
+let ls = JSON.parse(localStorage.getItem("Recent Searches"));
+let recentSearches = [];
+console.log(ls);
+if (ls === null) {
+  recentSearches = [
+    "Previous City",
+    "Previous City",
+    "Previous City",
+    "Previous City",
+    "Previous City",
+    "Previous City",
+    "Previous City"
+  ];
+} else {
+  recentSearches = ls;
+  for (i = 0; i < recentSearches.length; i++) {
+    $("#prev" + i).text(recentSearches[i]);
+  }
+}
+$(".btn-primary").click(function(event) {
+  event.preventDefault();
+  let input = $(".form-control").val();
+  setWeather(input);
+  for (i = recentSearches.length - 1; i > 0; i--) {
+    recentSearches[i] = recentSearches[i - 1];
+  }
+  recentSearches[0] = input;
+  localStorage.setItem("Recent Searches", JSON.stringify(recentSearches));
+  for (i = 0; i < recentSearches.length; i++) {
+    $("#prev" + i).text(recentSearches[i]);
+  }
+});
+$(".btn-secondary").click(function(event) {
+  event.preventDefault();
+  setWeather($("#" + event.target.id).text());
+});
+function setWeather(city) {
+  let queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+  fetch(queryURL).then(response => response.json()).then(data => {
+    let lat = data.coord.lat;
+    let lon = data.coord.lon;
+    let queryURL2 =
+      "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=1d19cda4c60cdb1c9b264ede49ac9a5e&units=imperial&exclude=hourly,minutely";
+    fetch(queryURL2).then(response => response.json()).then(data => {
+      for (i = 0; i <= 5; i++) {
+        let date = new Date(data.daily[i].dt * 1000);
+        let dateFormat = date.toLocaleDateString();
+        $("#" + i).children("h3").text(dateFormat);
+        $("#" + i).children(".temp").text("Temp: " + data.daily[i].temp.day);
+        $("#" + i).children(".wind").text("Wind: " + data.daily[i].wind_speed);
+        $("#" + i)
+          .children(".humidity")
+          .text("Humidity: " + data.daily[i].humidity + "%");
+        if (i === 0) {
+          $("#0").children("h2").text(city + " " + dateFormat);
+          $("#0").children(".UV").text("UV Index: " + data.daily[i].uvi);
+        }
+      }
+    });
+  });
+}
+setWeather("Minneapolis");
